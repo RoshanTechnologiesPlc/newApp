9e163a385bd9138794e578875322f14325e9e1c2
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2026-05-27 21:22:19 +0300

    Refactor response body reading method
    Replaced strings.Builder with io.ReadAll for response body handling.

--- backend/main.go
+++ backend/main.go
@@ -14,6 +14,7 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"io"
 
 	"github.com/gocolly/colly/v2"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -325,11 +326,12 @@ func resolveByGoogleHTML(rawURL string) string {
 	}
 	defer resp.Body.Close()
 
-	var builder strings.Builder
-	_, err = builder.ReadFrom(resp.Body)
-	if err != nil {
-		return ""
-	}
+	bodyBytes, err := io.ReadAll(resp.Body)
+if err != nil {
+	return ""
+}
+
+body := html.UnescapeString(string(bodyBytes))
 
 	body := html.UnescapeString(builder.String())
 	body = strings.ReplaceAll(body, `\/`, `/`)
